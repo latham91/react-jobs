7f0a2717c765981bf6d5beb18746e874342006fd
Aaron Latham <latham91@icloud.com>
2024-04-24 14:14:38 +0100

    enhancement: add link button to hero component

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,7 +7,7 @@ export default function CTA() {
         <div className="flex flex-col gap-5 items-start bg-[#364151] p-5 w-full rounded-md shadow-lg border-dashed border-2 border-white/10">
           <h3 className="text-3xl font-bold">For Developers</h3>
           <p>Browse React jobs and start your career today.</p>
-          <Link to="/jobs" className="bg-[#0A7EA4] py-3 px-5 rounded-md hover:bg-[#0a7da4cc]">
+          <Link to="/jobs" className="font-bold bg-[#0A7EA4] py-3 px-5 rounded-md hover:bg-[#0a7da4cc]">
             Browse
           </Link>
         </div>
@@ -15,7 +15,7 @@ export default function CTA() {
         <div className="flex flex-col gap-5 items-start bg-[#364151] p-5 w-full rounded-md shadow-lg border-dashed border-2 border-white/10">
           <h3 className="text-3xl font-bold">For Employers</h3>
           <p>Advertise available roles and find your perfect developer.</p>
-          <Link to="/add" className="bg-[#0A7EA4] py-3 px-5 rounded-md hover:bg-[#0a7da4cc]">
+          <Link to="/add" className="font-bold bg-[#0A7EA4] py-3 px-5 rounded-md hover:bg-[#0a7da4cc]">
             Add Job
           </Link>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Hero() {
   return (
     <section className="flex flex-col items-center w-full p-5 gap-3 text-white bg-[#23272F] h-[400px] justify-center">
       <h1 className="text-7xl font-extrabold">React Jobs</h1>
       <p className="text-2xl">Find the perfect react job for you.</p>
+      <Link to="/jobs" className="px-5 py-2 text-lg font-semibold bg-[#0A7EA4] hover:bg-[#0a7da4cc] rounded-md mt-5">
+        Browse Jobs
+      </Link>
     </section>
   );
 }
