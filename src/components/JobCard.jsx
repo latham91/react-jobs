2c76b8e7b90879d53dd643cc317f82b6649f1887
Aaron Latham <latham91@icloud.com>
2024-04-24 14:37:09 +0100

    enhancement: add company to job card

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -4,7 +4,7 @@ import { MapPinned, Home } from "lucide-react";
 export default function JobCard() {
   return (
     <div className="bg-white rounded-lg shadow-md p-5 flex flex-col gap-5">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-3">
         <div className="text-sm flex justify-between items-center">
           <div className="flex items-center gap-1">
             <Home size={14} />
@@ -15,12 +15,13 @@ export default function JobCard() {
             Manchester, UK
           </div>
         </div>
+        <h2 className="text-sm font-medium">Microsoft Corporation</h2>
         <h3 className="text-xl font-bold text-slate-800">Junior Front-End Developer</h3>
       </div>
       <div className="flex flex-col gap-2">
         <p className="line-clamp-3">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sed laborum explicabo assumenda accusantium
-          repudiandae sint alias, expedita laudantium sapiente ad fuga? Quas beatae deserunt consectetur vitae? Esse,
+          repudiandae sint alias expedita laudantium sapiente ad fuga? Quas beatae deserunt consectetur vitae? Esse,
           atque pariatur!
         </p>
         <Link to="/job/1" className="text-blue-600 hover:underline">
