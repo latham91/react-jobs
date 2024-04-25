c5ba8142e1482a4c482daa1e476b08d1dab20af0
Aaron Latham <latham91@icloud.com>
2024-04-25 09:29:46 +0100

    enhancement: create heading for add job page

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,3 +1,10 @@
 export default function AddJobPage() {
-  return <div>AddJobPage</div>;
+  return (
+    <main>
+      <div className="bg-[#23272F] text-white h-[200px] flex flex-col gap-4 items-center justify-center">
+        <h1 className="text-5xl font-extrabold">Add Job Listing</h1>
+        <p className="text-lg">Advertise your job listing and find your perfect candidate.</p>
+      </div>
+    </main>
+  );
 }
