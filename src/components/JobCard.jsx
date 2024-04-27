aa3973bb211e8ee283dd679b6358e5dcbf5f0ddc
Aaron Latham <latham91@icloud.com>
2024-04-27 08:32:15 +0100

    enhancement: create JobCard component

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -3,16 +3,16 @@ import { MapPinned, Home } from "lucide-react";
 
 export default function JobCard() {
   return (
-    <div className="bg-white rounded-lg shadow-md p-5 flex flex-col gap-5">
+    <div className="flex flex-col gap-5 p-5 bg-white rounded-lg shadow-md">
       <div className="flex flex-col gap-3">
-        <div className="text-sm flex justify-between items-center">
+        <div className="flex items-center justify-between text-sm">
           <div className="flex items-center gap-1">
             <Home size={14} />
             Full-time - Remote
           </div>
           <div className="flex items-center gap-1">
             <MapPinned size={14} />
-            Manchester, UK
+            Preston, UK
           </div>
         </div>
         <h2 className="text-sm font-medium">Microsoft Corporation</h2>
@@ -20,20 +20,19 @@ export default function JobCard() {
       </div>
       <div className="flex flex-col gap-2">
         <p className="line-clamp-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sed laborum explicabo assumenda accusantium
-          repudiandae sint alias expedita laudantium sapiente ad fuga? Quas beatae deserunt consectetur vitae? Esse,
-          atque pariatur!
+          We are looking for a Junior Front-End Developer to join our team. You will be working with a team of
+          developers to build high-quality, dynamic web applications.
         </p>
         <Link to="/job/1" className="text-blue-600 hover:underline">
           Read more
         </Link>
       </div>
       <div>
-        <span className="text-slate-700 font-bold">$24,000 - $30,000 / per annum</span>
+        <span className="font-bold text-slate-700">$24,000 - $30,000 / per annum</span>
       </div>
       <div>
         <Link to="/job/1">
-          <button className="bg-slate-800 text-white py-2 px-5 rounded-md">Apply Now</button>
+          <button className="px-5 py-2 text-white rounded-md bg-slate-800">Apply Now</button>
         </Link>
       </div>
     </div>
