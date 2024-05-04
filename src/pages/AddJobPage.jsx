86928a9552a17ea2b0afe830bcd523c34734d5f7
Aaron Latham <latham91@icloud.com>
2024-05-04 14:46:55 +0100

    chore: updated header on add job page

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -2,7 +2,7 @@ export default function AddJobPage() {
   return (
     <main>
       <div className="bg-[#23272F] text-white h-[200px] flex flex-col gap-4 items-center justify-center">
-        <h1 className="text-5xl font-extrabold">Add Job Listing</h1>
+        <h1 className="text-5xl font-extrabold">Add Your Job Listing</h1>
         <p className="text-lg">Advertise your job listing and find your perfect candidate.</p>
       </div>
     </main>
