cef5fb2418b6038225f183e901936ae4a3ce2eb4
Aaron Latham <latham91@icloud.com>
2024-05-05 13:29:55 +0100

    enhancement: create section for add job form

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -5,6 +5,9 @@ export default function AddJobPage() {
         <h1 className="text-5xl font-extrabold">Add Your Job Listing</h1>
         <p className="text-lg">Advertise your job listing and find your perfect candidate.</p>
       </div>
+      <section className="flex gap-5 px-5 py-10 mx-auto max-w-7xl">
+        <div className="w-full p-5 border-2 border-dashed rounded-md h-fit border-slate-800/20">form goes in here</div>
+      </section>
     </main>
   );
 }
