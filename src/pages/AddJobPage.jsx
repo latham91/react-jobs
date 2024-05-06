f4a385d9893919288faa989084bcb9993aded883
Aaron Latham <latham91@icloud.com>
2024-05-06 22:24:01 +0100

    chore: text update

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -6,7 +6,7 @@ export default function AddJobPage() {
         <p className="text-lg">Advertise your job listing and find your perfect candidate.</p>
       </div>
       <section className="flex gap-5 px-5 py-10 mx-auto max-w-7xl">
-        <div className="w-full p-5 border-2 border-dashed rounded-md h-fit border-slate-800/20">form goes in here</div>
+        <div className="w-full p-5 border-2 border-dashed rounded-md h-fit border-slate-800/20">form goes in here.</div>
       </section>
     </main>
   );
